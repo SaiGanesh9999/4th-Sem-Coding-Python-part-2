8ab870f23f5b8bce180a0c878abd7093da6506d7
SaiGanesh9999 <110873642+SaiGanesh9999@users.noreply.github.com>
2023-05-02 07:28:33 +0530

    Create student_dictionary_program.py

---
+++ student_dictionary_program.py
@@ -0,0 +1,13 @@
+dic = {}
+n = int(input("Enter Number of students"))
+for i in range(n):
+    stunum = int(input("Enter Student Number: "))
+    k = {
+        "sname" : input("Enter Student Name: "),
+        "group" : input("Enter Student Group: "),
+        "college" : input("Enter Student College: "),
+        "branch" : input("Enter Student Branch: "),
+        "phone" : input("Enter Phone Number: ")
+    }
+    dic[stunum] = k
+print(dic[int(input("Enter Student Number: "))])
