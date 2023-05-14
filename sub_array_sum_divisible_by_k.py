2c46be4efb9a3489b82fa891d5e79aeff3e8c9b0
DEVARASETTI SAI GANESH <110873642+SaiGanesh9999@users.noreply.github.com>
2023-05-14 07:09:56 +0530

    sub_array_sum_divisible_by_k.py

---
+++ sub_array_sum_divisible_by_k.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def subarraysDivByK(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        summ = 0
+        res = 0
+        dic = {0:1}
+        for a in A:
+            summ = (summ + a) % K
+            if summ in dic:
+                res += dic[summ]
+                dic[summ] += 1
+            else:
+                dic[summ] = 1
+        return res
